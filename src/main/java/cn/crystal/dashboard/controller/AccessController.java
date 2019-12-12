b5507a45034489b1f759e30e6f1b05829f135aa4
陈之晶 <crystal_471006350@126.com>
2019-12-13 00:27:17 +0800

    share to git first

--- src/main/java/cn/crystal/dashboard/controller/AccessController.java
+++ src/main/java/cn/crystal/dashboard/controller/AccessController.java
@@ -4,6 +4,7 @@ import cn.crystal.dashboard.model.Access;
 import cn.crystal.dashboard.service.AccessService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -24,10 +25,18 @@ public class AccessController {
     @Autowired
     AccessService accessService;
 
+    @RequestMapping("accessIndex")
+    public String selectAllAccess(Model model){
+        List<Access> accesses = accessService.selectAllAccess();
+        model.addAttribute("accesses",accesses);
+        return "accessIndex";
+    }
+
     @ResponseBody
     @RequestMapping("selectAllAccess")
     public List<Access> selectAllAccess(){
-        return accessService.selectAllAccess();
+        List<Access> accesses = accessService.selectAllAccess();
+        return accesses;
     }
 
     @ResponseBody
